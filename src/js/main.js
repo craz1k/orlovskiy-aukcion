9de736b8c33456724eebf071910f9eb438691c71
nomad8t <serega18t@tut.by>
2020-10-06 16:37:29 +0300

    refactor: code structure

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+/* eslint-disable curly */
+/* eslint-disable radix */
+/* eslint-disable indent */
 let countdown;
 let secondsGlobal = 600;
 let id = 0;
@@ -14,7 +17,7 @@ const LOT_ARRAY = [];
 // const TIMER_DOM = document.querySelector('[timer]');
 const MIN_DOM = document.querySelector('[minutes]');
 const SEC_DOM = document.querySelector('[seconds]');
-const MSEC_DOM = document.querySelector('[mseconds]');
+// const MSEC_DOM = document.querySelector('[mseconds]');
 
 const START_STOP_BTN = document.querySelector('[start]');
 const RESET_BTN = document.querySelector('[reset]');
@@ -24,7 +27,6 @@ const EQUAL_TEN_MIN_BTN = document.querySelector('[ten-min]');
 const MINUS_ONE_MIN_BTN = document.querySelector('[minus-one-min]');
 const ADD_LOT_BTN = document.querySelector('[add-lot]');
 const LOTS_DOM = document.querySelector('.auc__lots-wrapper');
-let lotsItemDOM = document.querySelectorAll('.auc__item');
 
 const startTimer = (seconds) => {
   const NOW = Date.now();
@@ -83,23 +85,55 @@ const addLot = () => {
 };
 
 const sortLots = (arr) => {
+  const arrLength = arr.length;
+  if (arr[arrLength - 1] < arr[arrLength - 2]) return;
   arr.sort((a, b) => {
     return b.totalBet - a.totalBet;
   });
 };
 
 const displayLots = (arr) => {
-  for (let i = 0; i < lotsItemDOM.length; i++) {
-    lotsItemDOM[i].childNodes[0].value = '';
-    lotsItemDOM[i].childNodes[1].value = '';
-    lotsItemDOM[i].childNodes[2].value = '';
-    lotsItemDOM[i].childNodes[3].removeAttribute('id');
+  const LOTS_ITEM_DOM = document.querySelectorAll('.auc__item');
+  const ARR_LENGTH = arr.length;
+  const LOTS_ITEM_DOM_LENGTH = LOTS_ITEM_DOM.length;
+
+  for (let i = 0; i < LOTS_ITEM_DOM_LENGTH; i++) {
+    LOTS_ITEM_DOM[i].childNodes[0].value = '';
+    LOTS_ITEM_DOM[i].childNodes[1].value = '';
+    LOTS_ITEM_DOM[i].childNodes[2].value = '';
+    LOTS_ITEM_DOM[i].childNodes[3].removeAttribute('id');
   }
 
-  for (let i = 0; i < arr.length; i++) {
-    lotsItemDOM[i].childNodes[0].value = arr[i].name;
-    lotsItemDOM[i].childNodes[1].value = arr[i].totalBet;
-    lotsItemDOM[i].childNodes[3].setAttribute('id', arr[i].id);
+  for (let i = 0; i < ARR_LENGTH; i++) {
+    LOTS_ITEM_DOM[i].childNodes[0].value = arr[i].name;
+    LOTS_ITEM_DOM[i].childNodes[1].value = arr[i].totalBet;
+    LOTS_ITEM_DOM[i].childNodes[3].setAttribute('id', arr[i].id);
+  }
+};
+
+const lotArrayFill = (name, totalBet, lastBet) => {
+  const LOT_CLONE = {};
+  Object.assign(LOT_CLONE, LOT);
+
+  LOT_CLONE.id = id++;
+  LOT_CLONE.name = name;
+  if (isNaN(parseFloat(totalBet))) {
+    LOT_CLONE.totalBet += parseFloat(lastBet);
+  } else {
+    LOT_CLONE.totalBet = parseFloat(lastBet) + parseFloat(totalBet);
+  }
+  LOT_CLONE.lastBet = parseFloat(lastBet);
+  LOT_ARRAY.push(LOT_CLONE);
+};
+
+const lotArrayEdit = (idEd, name, totalBet, lastBet) => {
+  const curLot = LOT_ARRAY.find((el) => el.id === idEd);
+  curLot.name = name;
+  curLot.lastBet = parseFloat(lastBet);
+  if (isNaN(parseFloat(totalBet))) {
+    curLot.totalBet += parseFloat(lastBet);
+  } else {
+    curLot.totalBet = parseFloat(lastBet) + parseFloat(totalBet);
   }
 };
 
@@ -135,8 +169,6 @@ MINUS_ONE_MIN_BTN.addEventListener('click', () => {
 ADD_LOT_BTN.addEventListener('click', addLot);
 
 const eventListenerAdding = () => document.querySelectorAll('[add-sum]').forEach((btn) => {
-  lotsItemDOM = document.querySelectorAll('.auc__item');
-
   btn.addEventListener('click', () => {
     const LOT_NAME_DOM = btn.parentElement.firstChild;
     const LOT_CUR_SUM_DOM = btn.previousSibling;
@@ -144,40 +176,21 @@ const eventListenerAdding = () => document.querySelectorAll('[add-sum]').forEach
 
     if (!isNaN(parseFloat(LOT_CUR_SUM_DOM.value))) {
       if (!btn.hasAttribute('id')) {
-        const LOT_CLONE = {};
-        Object.assign(LOT_CLONE, LOT);
-
-        LOT_CLONE.id = id++;
-        LOT_CLONE.name = LOT_NAME_DOM.value;
-        if (isNaN(parseFloat(LOT_TOTAL_SUM_DOM.value))) {
-          LOT_CLONE.totalBet += parseFloat(LOT_CUR_SUM_DOM.value);
-        } else {
-          LOT_CLONE.totalBet = parseFloat(LOT_CUR_SUM_DOM.value) + parseFloat(LOT_TOTAL_SUM_DOM.value);
-        }
-        LOT_CLONE.lastBet = parseFloat(LOT_CUR_SUM_DOM.value);
-
-        LOT_CUR_SUM_DOM.value = '';
-        LOT_TOTAL_SUM_DOM.value = LOT_CLONE.totalBet;
-
-        LOT_ARRAY.push(LOT_CLONE);
-        sortLots(LOT_ARRAY);
-        displayLots(LOT_ARRAY);
-        console.log(LOT_ARRAY);
+        lotArrayFill(
+          LOT_NAME_DOM.value,
+          LOT_TOTAL_SUM_DOM.value,
+          LOT_CUR_SUM_DOM.value
+        );
       } else {
-        const curLot = LOT_ARRAY.find((el) => el.id === parseInt(btn.getAttribute('id'), 10));
-        curLot.name = LOT_NAME_DOM.value;
-        curLot.lastBet = parseFloat(LOT_CUR_SUM_DOM.value);
-        if (isNaN(parseFloat(LOT_TOTAL_SUM_DOM.value))) {
-          curLot.totalBet += parseFloat(LOT_CUR_SUM_DOM.value);
-        } else {
-          curLot.totalBet = parseFloat(LOT_CUR_SUM_DOM.value) + parseFloat(LOT_TOTAL_SUM_DOM.value);
-        }
-        LOT_CUR_SUM_DOM.value = '';
-        LOT_TOTAL_SUM_DOM.value = curLot.totalBet;
-        sortLots(LOT_ARRAY);
-        displayLots(LOT_ARRAY);
-        console.log(LOT_ARRAY);
+        lotArrayEdit(
+          parseInt(btn.getAttribute('id')),
+          LOT_NAME_DOM.value,
+          LOT_TOTAL_SUM_DOM.value,
+          LOT_CUR_SUM_DOM.value
+        );
       }
+      sortLots(LOT_ARRAY);
+      displayLots(LOT_ARRAY);
     }
   });
 });
